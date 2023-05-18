948e6da41d4f1a7e10b306009b509a4c798d15a1
kuang_hao@foxmail.com <kuang_hao@foxmail.com>
2023-05-18 16:57:18 +0800

    1

---
+++ Map/config/global.config.js
@@ -0,0 +1,16 @@
+/**
+ * @Description:
+ * @author Mr.Kuang
+ * @date 2023/1/29 0029
+*/
+const g_config={
+    systemTitle: '全天候多模式森林火险监测预报系统',
+    tdtToken :"970717ce310864de2ce5aaf014936e2c",
+    cesiumToken :"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJjNWMwYWU4ZS1mYjJhLTRiODEtOTVkYS0zY2Y3NzRjMTI2OTUiLCJpZCI6MzM1MjAsImlhdCI6MTYwNTAwMTA2Mn0.BiUHe6nuySvtQAiIIwMWSHjPyu0iD1mvw-8otfVkNHs",
+    globalViewer: null,
+    olMap:{
+        map:null,
+        view:null
+    }
+
+}
